d2a5927d1cf164e2c343e36e6de135b645920510
Sebasdsk <80434951+Sebasdsk@users.noreply.github.com>
2022-09-11 23:57:29 -0600

    trabajos progra

---
+++ mini test 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Introduzca un numero: "  ;
+    int x {0};
+    cin >> x;
+    cout << "Ingresaste: " << x;
+    return 0;
+}
---
+++ multiplica x 2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Ingrese un entero" << endl;
+    int num;
+    cin >> num;
+    cout << "El doble del numero es: " << (num*2);
+
+    return 0;
+}
